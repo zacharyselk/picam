20b4dfdf03fb4490d0c44ad8d0e5128a81c57b9d
zacharyselk <zrselk@gmail.com>
2018-05-14 12:52:56 -0600

    More data

--- scripts/run.cc
+++ scripts/run.cc
@@ -2,7 +2,7 @@
 
 int main()
 {
-  system("./write_timestamps.py 1800 120 480p_singlerecord_cc h264");
+  system("./write_timestamps.py 900 120 480p_singlerecord_cc h264");
 
   return 0;
 }
---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+./write_timestamps.py 900 120 480p_singlerecord_bash h264
