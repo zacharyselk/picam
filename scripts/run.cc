9b617d9e1d5382f201076ab7127f7fd88b6da58f
zacharyselk <zrselk@gmail.com>
2018-05-14 11:43:43 -0600

    More data

---
+++ scripts/run.cc
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+
+int main()
+{
+  system("./write_timestamps.py 1800 120 480p_singlerecord_cc h264");
+
+  return 0;
+}
---
+++ scripts/run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('./write_timestamps.py 1800 120 480p_singlerecord_python h264')
